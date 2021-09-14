1b390bff9c5d1129d0cf5a76f2a5e4c6d0e9c01d
Joshua French <just-joshing@github.com>
2021-09-14 13:27:10 -0600

    Setup p10k

--- setup.sh
+++ setup.sh
@@ -2,7 +2,12 @@
 
 ./setup-git.sh
 
+# Copy dotfiles
 mv ~/.bashrc ~/.bashrc.bak
 mv ~/.zshrc ~/.zshrc.bak
 cp ./.bashrc ~/
 cp ./.zshrc ~/
+
+# Setup prompt
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
