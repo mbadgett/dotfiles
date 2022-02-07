6124a3edf8640d6f0564bb055bd3e13bad631ba7
Michael Badgett <mbadgett@github.com>
2022-02-07 09:41:10 -0800

    Update setup-git.sh

--- setup-git.sh
+++ setup-git.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-git config --global user.name "Joshua French"
-git config --global user.email "just-joshing@github.com"
-git config --global github.user just-joshing
+git config --global user.name "Michael Badgett"
+git config --global user.email "mbadgett@github.com"
+git config --global github.user mbadgett
 git config --global core.editor "code --wait"
 git config --global push.default current
 
